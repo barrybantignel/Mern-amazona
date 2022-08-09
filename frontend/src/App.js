f2fdb3fa57f95aa35ad6f205bd597da356ce606b
barrybantignel <barrybantignel@gmail.com>
2022-08-09 16:32:55 +0200

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        Liste des produits       
+      </main>
+    </div>
+  );
+}
+
+export default App;
